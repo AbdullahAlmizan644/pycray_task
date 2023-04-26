121d283f7458865a440061e7c854fb7b1b9e10b8
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2023-04-26 10:56:07 +0600

    db

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+#Database Username
+username="root"
+#Database Password
+password=""
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/task'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@localhost/task'
 db = SQLAlchemy(app) 
 db.init_app(app)
 
